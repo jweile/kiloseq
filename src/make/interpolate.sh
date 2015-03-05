9e975c9664f8b06c83cafdc2c1c18ed01f1476b6
jochenweile <jochenweile@gmail.com>
2015-03-05 10:50:48 -0500

    updated build scripts

--- src/make/interpolate.sh
+++ src/make/interpolate.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 ###
 #
@@ -15,39 +16,42 @@ buildDir=$2
 
 #read bin locations from cfg.bin
 declare -A bins=(
-	["Rbin"]=`grep Rscript $config|cut -d, -f2` 
-	["BLASTbin"]=`grep blast $config|cut -d, -f2` 
-	["SGEbin"]=`grep Rscript $config|cut -d, -f2`
+  ["Rbin"]=`grep Rscript $config|cut -d, -f2` 
+  ["BLASTbin"]=`grep blast $config|cut -d, -f2` 
+  ["SGEbin"]=`grep Rscript $config|cut -d, -f2`
+  ["BowtieBin"]=`grep bowtie2 $config|cut -d, -f2`
 )
 
 function interpolate {
-	
-	targetfile=$1
+  
+  targetfile=$1
 
-	#Check if file exists
-	if [ ! -r $targetfile ] 
-	then
-		echo "Cannot read file $targetfile!"
-		exit 1
-	fi
+  #Check if file exists
+  if [ ! -r $targetfile ] 
+  then
+    echo "Cannot read file $targetfile!"
+    exit 1
+  fi
 
-	#Load file contents into variable
-	contents=`cat $targetfile`
+  #Load file contents into variable
+  contents=`cat $targetfile`
 
-	#iterator over remaining arguments
-	for key in ${!bins[@]}
-	do
-		#escape slash characters (don't even think about messing with this!)
-		value=`echo ${bins[$key]}|sed -e 's/\//\\\\\//g'`
-		#replace current variable with its value
-		contents=`echo "$contents"|sed "s/\\\$$key/$value/g"`
-		# echo "$key => ${bins[$key]}"
-	done
+  #iterator over remaining arguments
+  for key in ${!bins[@]}
+  do
+    #escape slash characters (don't even think about messing with this!)
+    value=`echo ${bins[$key]}|sed -e 's/\//\\\\\//g'`
+    #replace current variable with its value
+    contents=`echo "$contents"|sed "s/\\\$$key/$value/g"`
+    # echo "$key => ${bins[$key]}"
+  done
 
-	#return results
-	echo "$contents">$targetfile
+  #return results
+  echo "$contents">$targetfile
 }
 
-interpolate "$buildDir/lib/analyze2.R"
+# interpolate "$buildDir/lib/analyze2.R"
 interpolate "$buildDir/lib/master.R"
-interpolate "$buildDir/lib/slave.R"
+interpolate "$buildDir/lib/demuxer.R"
+interpolate "$buildDir/lib/consolidateAndAlign.R"
+interpolate "$buildDir/lib/libyogiseq.R"
